179c480626d0839092f78ef202f87e2ae4c29373
aias_for_win10 <aiassing@outlook.com>
2016-08-22 21:57:12 +0800

    try complete for DoubleLinkedList

--- src/main/Unit1/DoubleLinkedList.java
+++ src/main/Unit1/DoubleLinkedList.java
@@ -1,12 +1,21 @@
 package Unit1;
 
+import java.nio.file.NotDirectoryException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import Unit1.Queue.Node;
-
+/**
+ * @author Administrator
+ *
+ * @param <Item>
+ */
+/**
+ * @author Administrator
+ *
+ * @param <Item>
+ */
 public class DoubleLinkedList<Item> implements Iterable<Item> {
 	private Node<Item> first;
 	private Node<Item> last;
@@ -18,19 +27,18 @@ public class DoubleLinkedList<Item> implements Iterable<Item> {
 		private Node<Item> next;
 	}
 
-	// 如果链表为空去添加 函数
-	// private void ifIsEmptyToIncrease(Item item){
-	// if(isEmpty()){
-	// // 如果链表为空。那么它就是唯一的结点——首结点是它，尾结点也是它。
-	// first.previous = null;
-	// first.item = item;
-	// first.next = null;
-	// last.previous = null;
-	// last.item = item;
-	// last.next = null;
-	// n++;
-	// }
-	// }
+	public void isHadNode(Node node) {
+
+		Node<Item> nowNode = first;
+		for (int i = 1; i == n; i++) {
+			if (nowNode.equals(node)) {
+				break;
+			} else {
+				nowNode = first.next;
+			}
+		}
+		throw new NoSuchElementException("can't find this node!");
+	}
 
 	// 在表头插入结点
 	public void insterAtFirst(Item item) {
@@ -84,7 +92,7 @@ public class DoubleLinkedList<Item> implements Iterable<Item> {
 	public void deleteAtFirst() {
 		// 如果为空，则不能正常删除，抛出异常
 		if (first == null) {
-			throw new NoSuchElementException("DoubleLinkedList underflow");
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
 		}
 		if (n == 1) {
 			first = null;
@@ -102,7 +110,7 @@ public class DoubleLinkedList<Item> implements Iterable<Item> {
 	// 从表尾删除结点
 	public void deleteAtLast() {
 		if (first == null) {
-			throw new NoSuchElementException("DoubleLinkedList underflow");
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
 		}
 		if (n == 1) {
 			first = null;
@@ -116,40 +124,103 @@ public class DoubleLinkedList<Item> implements Iterable<Item> {
 			n--;
 		}
 	}
-//以下方法都没有对特殊情况进行判断，比如数量为0、1等...
-	// 在指定结点前插入
-	public void insertBefore(Node<Item> node,Node<Item> newNode){
-		//处理前面的指向
+
+
+	/**
+	 * 在指定结点前插入
+	 * 
+	 * @param node
+	 * @param newNode
+	 */
+	public void insertBefore(Node<Item> node, Node<Item> newNode) {
+		if (first == null) {
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
+		}
+		isHadNode(node);
+		// 判断：只有一个结点
+		if (n==1){
+			first.previous = newNode;
+			newNode.next = first;
+		}
+
+		// 处理前面的指向
 		newNode.previous = node.previous;
 		node.previous.next = newNode;
-		//处理后面的指向
+		// 处理后面的指向
 		newNode.next = node;
-		node.previous = newNode;	
+		node.previous = newNode;
 		n++;
 	}
-	// 在指定结点后插入
-	public void insertAfter(Node<Item>node,Node<Item>newNode){
-		//处理后面的指向
+
+	/**
+	 * 在指定结点后插入
+	 * 
+	 * @param node
+	 * @param newNode
+	 */
+	public void insertAfter(Node<Item> node, Node<Item> newNode) {
+		if (first == null) {
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
+		}
+		isHadNode(node);
+		// 判断：只有一个结点
+		if (n==1){
+			newNode.previous = first;
+			first.next = newNode;
+		}
+		// 处理后面的指向
 		newNode.next = node.next;
-		node.next.previous =newNode;
-		//处理前面的指向
+		node.next.previous = newNode;
+		// 处理前面的指向
 		newNode.previous = node;
 		node.next = newNode;
 		n++;
 	}
-	// 从指定结点前删除
-	public void deleteBefore(Node<Item>node){
-		//前一个节点指向前前一个
+
+	/**
+	 * 删除指定结点
+	 * 
+	 * @param node
+	 */
+	public void deleteNode(Node<Item> node) {
+		if (first == null) {
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
+		}
+		isHadNode(node);
+		node.previous.next = node.next;
+		node.next.previous = node.previous;
+		node = null;
+	}
+
+	/**
+	 * 从指定结点前删除
+	 * 
+	 * @param node
+	 */
+	public void deleteBefore(Node<Item> node) {
+		if (first == null) {
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
+		}
+		isHadNode(node);
+		// 前一个节点指向前前一个
 		node.previous = node.previous.previous;
-		node.previous.next = node;	
+		node.previous.next = node;
 		n--;
 	}
-	// 从指定结点后删除
-	public void deleteAfter(Node<Item>node){
+
+	/**
+	 * 从指定结点后删除
+	 * 
+	 * @param node
+	 */
+	public void deleteAfter(Node<Item> node) {
+		if (first == null) {
+			throw new NoSuchElementException("DoubleLinkedList is Empty");
+		}
+		isHadNode(node);
 		node.next = node.next.next;
 		node.next.previous = node;
-		n --;
-		List <Item>list = new ArrayList();
+		n--;
 	}
 
 	public boolean isEmpty() {
@@ -168,6 +239,7 @@ public class DoubleLinkedList<Item> implements Iterable<Item> {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	private class ListIterator<Item> implements Iterator<Item> {
 		private Node<Item> current;
 
@@ -175,12 +247,12 @@ public class DoubleLinkedList<Item> implements Iterable<Item> {
 			current = first;
 		}
 
-		//必须被实现的方法1
+		// 必须被实现的方法1
 		public boolean hasNext() {
 			return current != null;
 		}
 
-		//必须被实现的方法2
+		// 必须被实现的方法2
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
