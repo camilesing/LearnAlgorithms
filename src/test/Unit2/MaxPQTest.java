00cacd033492ab5ed39f08a708f710e4747153ac
aias_for_win10 <aiassing@outlook.com>
2016-09-26 23:39:56 +0800

    尝试优先队列

---
+++ src/test/Unit2/MaxPQTest.java
@@ -0,0 +1,20 @@
+package Unit2;
+
+import org.junit.Test;
+
+import Tools.StdIn;
+import Tools.StdOut;
+
+public class MaxPQTest {
+
+	@Test
+	public void Test(){
+        MaxPQ<String> pq = new MaxPQ<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) pq.insert(item);
+            else if (!pq.isEmpty()) StdOut.print(pq.delMax() + " ");
+        }
+        StdOut.println("(" + pq.size() + " left on pq)");
+	}
+}
