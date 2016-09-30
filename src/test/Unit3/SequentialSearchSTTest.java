e1071b42398d1754c7f2a599b7eec192b0348a03
aias_for_win10 <aiassing@outlook.com>
2016-09-30 23:54:19 +0800

    fixed error and typesetting

--- src/test/Unit3/SequentialSearchSTTest.java
+++ src/test/Unit3/SequentialSearchSTTest.java
@@ -1,17 +1,18 @@
 package Unit3;
 
 import org.junit.Test;
+import Tools.*;
 
 public class SequentialSearchSTTest {
 
 	@Test
-	public void Test(){
-        SequentialSearchST<String, Integer> st = new SequentialSearchST<String, Integer>();
-        for (int i = 0; !StdIn.isEmpty(); i++) {
-            String key = StdIn.readString();
-            st.put(key, i);
-        }
-        for (String s : st.keys())
-            StdOut.println(s + " " + st.get(s));
+	public void Test() {
+		SequentialSearchST<String, Integer> st = new SequentialSearchST<String, Integer>();
+		for (int i = 0; !StdIn.isEmpty(); i++) {
+			String key = StdIn.readString();
+			st.put(key, i);
+		}
+		for (String s : st.keys())
+			StdOut.println(s + " " + st.get(s));
 	}
 }
