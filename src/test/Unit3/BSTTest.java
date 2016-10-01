9d19d816fbd3026689d836dae3366ec53545e3e9
aias_for_win10 <aiassing@outlook.com>
2016-10-01 23:09:55 +0800

    尝试二叉查找树

---
+++ src/test/Unit3/BSTTest.java
@@ -0,0 +1,26 @@
+package Unit3;
+
+import org.junit.Test;
+
+import Tools.StdIn;
+import Tools.StdOut;
+
+public class BSTTest {
+
+	@Test
+	public void Test(){
+        BST<String, Integer> st = new BST<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        for (String s : st.levelOrder())
+            StdOut.println(s + " " + st.get(s));
+
+        StdOut.println();
+
+        for (String s : st.keys())
+            StdOut.println(s + " " + st.get(s));
+	}
+}
