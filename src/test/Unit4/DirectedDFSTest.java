ae77592544e5adb963b753b52eafc5e3870643af
aias_for_win10 <aiassing@outlook.com>
2016-10-10 20:24:21 +0800

    尝试有向图的深度优先查找

---
+++ src/test/Unit4/DirectedDFSTest.java
@@ -0,0 +1,31 @@
+package Unit4;
+
+import org.junit.Test;
+import Tools.*;
+import Unit1.Bag;
+
+public class DirectedDFSTest {
+	@Test
+    public  void Test(String[] args) {
+
+        // read in digraph from command-line argument
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+
+        // read in sources from command-line arguments
+        Bag<Integer> sources = new Bag<Integer>();
+        for (int i = 1; i < args.length; i++) {
+            int s = Integer.parseInt(args[i]);
+            sources.add(s);
+        }
+
+        // multiple-source reachability
+        DirectedDFS dfs = new DirectedDFS(G, sources);
+
+        // print out vertices reachable from sources
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.marked(v)) StdOut.print(v + " ");
+        }
+        StdOut.println();
+    }
+}
