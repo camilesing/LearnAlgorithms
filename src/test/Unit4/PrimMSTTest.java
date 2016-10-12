7c40b8522f36b6412cf0d3e041f60f964b9eae22
aias_for_win10 <aiassing@outlook.com>
2016-10-12 20:57:42 +0800

    尝试Prim的即时实现算法

---
+++ src/test/Unit4/PrimMSTTest.java
@@ -0,0 +1,19 @@
+package Unit4;
+
+import org.junit.Test;
+import Tools.*;
+
+public class PrimMSTTest {
+
+	@Test
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        PrimMST mst = new PrimMST(G);
+        for (Edge e : mst.edges()) {
+            StdOut.println(e);
+        }
+        StdOut.printf("%.5f\n", mst.weight());
+    }
+
+}
