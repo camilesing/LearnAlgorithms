79628f95502d637013bd9e6c5d846f508e5f0b36
aias_for_win10 <aiassing@outlook.com>
2016-10-13 15:24:11 +0800

    尝试KruskalMST算法

---
+++ src/test/Unit4/KruskalMSTTest.java
@@ -0,0 +1,18 @@
+package Unit4;
+import org.junit.*;
+import Tools.*;
+
+
+public class KruskalMSTTest {
+
+	@Test
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        KruskalMST mst = new KruskalMST(G);
+        for (Edge e : mst.edges()) {
+            StdOut.println(e);
+        }
+        StdOut.printf("%.5f\n", mst.weight());
+    }
+}
